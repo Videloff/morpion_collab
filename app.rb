75e9acac122f4fe0d1d09852b89acf3ea3306498
vincent <kratos_angel_59@hotmail.fr>
2023-04-27 10:57:03 +0200

    initialize repo

---
+++ app.rb
@@ -0,0 +1,27 @@
+# require 'bundler'
+# Bundler.require
+
+# $:.unshift File.expand_path("./../lib", __FILE__)
+require_relative 'lib/player'
+
+def get_names
+  puts "La partie de morpion va commencer, mais avant, veuillez choisir les pseudos des deux joueurs !"
+  names = []
+  print "Choisissez un pseudo pour le joueur 1\n >> "
+  names[0] = gets.chomp + "-j1"
+  print "Choisissez un pseudo pour le joueur 2\n >> "
+  names[1] = gets.chomp + "-j2"
+
+  return names
+end
+
+def main
+  names = get_names
+  joueur1 = Player.new(names[0], "x")
+  joueur2 = Player.new(names[1], "y")
+  puts "-----"
+  puts joueur1.name
+  puts joueur2.name
+end
+
+main
---
+++ lib/player.rb
@@ -0,0 +1,9 @@
+class Player
+  attr_accessor :name, :symbol
+
+  def initialize(name_to_save, symbol_mark)
+    @name = name_to_save
+    @symbol = symbol_mark
+  end
+
+end
